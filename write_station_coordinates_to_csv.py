4665140579f9c65ca19a0462fb7d158cc1b35e6c
Meia Alsup <meia.alsup@gmail.com>
2018-11-18 18:29:29 -0500

    flip lat long

--- write_station_coordinates_to_csv.py
+++ write_station_coordinates_to_csv.py
@@ -7,10 +7,14 @@ station_coordinates = get_station_coordinates()
 
 f = open('station_coordinates.csv', 'w')
 with f:
-    fields = ['station_id', 'latitude', 'longitude']
+    fields = ['station_id', 'longitude','latitude']
     writer = csv.DictWriter(f, fieldnames=fields)
     writer.writeheader()
     for id_, coords in station_coordinates.items():
         if coords[0] > 0:
-            writer.writerow({'station_id': id_, 'latitude': coords[0], 'longitude': coords[1]})
+            writer.writerow({
+                'station_id': id_,
+                'longitude': coords[1],
+                'latitude': coords[0], 
+            })
 
