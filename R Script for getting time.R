fa66e6afb56cef90b5d8fc4c31efda18fa7d06c1
Yue Wu <32078120+wuyueqz@users.noreply.github.com>
2018-11-18 13:43:14 -0500

    Update R Script for getting time.R

--- R Script for getting time.R
+++ R Script for getting time.R
@@ -1,5 +1,5 @@
 ## Get travel time by destination and different mode
-
+## Load package gmapsdistance
 install.packages("gmapsdistance")
 library("gmapsdistance")
 
@@ -10,13 +10,13 @@ set.api.key("AIzaSyCJnz9dwMTva-N8xTMC-MQGgFQRnqK4Hh8")
 results <- gmapsdistance(origin = "Washington+DC",
                          destination = "New+York+City+NY",
                          mode = "driving")
+results
 
 ## Sample 02
-origin = c("40.431478+-80.0505401", "33.7678359+-84.4906438")
-destination = c("43.0995629+-79.0437609", "41.7096483+-86.9093986")
+origin <- c("40.431478+-80.0505401", "33.7678359+-84.4906438")
+destination <- c("43.0995629+-79.0437609", "41.7096483+-86.9093986")
 results <- gmapsdistance(origin,
                         destination,
                         mode = "bicycling",
                         shape="long")
-
 results
