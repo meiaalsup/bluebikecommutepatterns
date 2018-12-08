66f3f44d65fb5b1f7d4585e07fd17b34d5f94b66
Meia Alsup <meia.alsup@gmail.com>
2018-12-08 18:32:05 -0500

    Continue analysis for most relevant and frequently used routes

--- pull_data.py
+++ pull_data.py
@@ -5,3 +5,9 @@ gmaps = googlemaps.Client(key=API_KEY)
 
 res = gmaps.distance_matrix((42.35044, -71.08945), (42.35083, -71.08981), mode='walking')
 print(res)
+
+
+# load in the data points 
+
+for route in [(67, 179), (179, 80), (178, 80), (67, 53), (53, 67)]:
+    
