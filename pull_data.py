abd100ea0a379d2014bc1f39fcf70a28f261aac6
Meia Alsup <meia.alsup@gmail.com>
2018-12-09 04:31:52 -0500

    full data sets for sampled transit

--- pull_data.py
+++ pull_data.py
@@ -2,51 +2,55 @@ import math
 
 import pandas as pd
 import googlemaps
+import json
+import csv
 
 from api_key import API_KEY
 
 gmaps = googlemaps.Client(key=API_KEY)
 
 
-time = gmaps.distance_matrix(
-            start_ls,
-            end_ls,
-            mode='driving',
-            traffic_model='optimistic',
-            departure_time=1544000018,
-        )
+routes = [
+    (64, 190),
+    (39, 22), (189, 190), (67, 74), (22, 190), (80, 78), (39, 46), (133, 124), (4, 22),
+    (80, 190)]
+num_to_sample = 3
 
-# load in the data points 
+def sample(mode='driving'):
+    full_output = {}
+    all_times = {}
+    for route in routes:
+        start, end = route
+        start_points = pd.read_csv(f'sample_points/r{start}.csv', header=0)[:200]
+        end_points = pd.read_csv(f'sample_points/r{end}.csv', header=0)[:200]
+        num_to_sample = start_points.shape[0]
+        times = []
+        output = []
+        for i in range(num_to_sample):
+            time = gmaps.distance_matrix(
+                (start_points['y'][i],start_points['x'][i]),
+                (end_points['y'][i], end_points['x'][i]),
+                mode=mode,
+                #traffic_model='optimistic',
+            )
+            if 'duration' in time['rows'][0]['elements'][0]:
+                times.append(time['rows'][0]['elements'][0]['duration']['value'])
+            else:
+                times.append(0)
+            output.append(time)
+     
+        all_times[route] = times
+        full_output[str(route)] = output
+    writer = csv.DictWriter(
+        open(f'{mode}_times.csv', 'w'),
+        fieldnames=[str(r) for r in routes]
+    )  
+    writer.writeheader()
 
-all_times = {}
-for route in [(64, 190), (39, 22), (189, 190), (67, 74), (22, 190), (80, 78), (39, 46), (133, 124),
-              (4, 22), (80, 190)]:
-    start, end = route
-    start_points = pd.read_csv(f'sample_points/r{start}.csv', header=0)
-    end_points = pd.read_csv(f'sample_points/r{end}.csv', header=0)
-    num_points = start_points.shape[0]
-    num_splits = math.ceil(num_points/100)
-    print(num_points)
-    print(num_splits)
-    times = []
-    for j in range(num_splits):
-        start_ls = []
-        end_ls = []
-        for i in range(100):
-            start_ls.append((start_points['x'][100*j+i],start_points['y'][100*j+i]))
-            end_ls.append((end_points['x'][100*j+i], end_points['y'][100*j+i]))
+    for j in range(num_to_sample):
+        row = {str(r): all_times[r][j] for r in routes}
+        writer.writerow(row)
+    json.dump(full_output, open(f'{mode}_routes_full_output', 'w'))
 
-        time = gmaps.distance_matrix(
-            start_ls,
-            end_ls,
-            mode='driving',
-            traffic_model='optimistic',
-            departure_time=1544000018,
-        )
-        times.append(time)
-        break
-
-    all_times[route] = times
-
-    
-
+sample('driving')
+sample('transit')
