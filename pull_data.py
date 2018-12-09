bc3cebb0c90ddd7e6f9d69e1a6981afeaf987ebd
Meia Alsup <meia.alsup@gmail.com>
2018-12-08 22:24:19 -0500

    playing around with pulling the transit time from google maps distance matrix

--- pull_data.py
+++ pull_data.py
@@ -1,13 +1,52 @@
+import math
+
+import pandas as pd
 import googlemaps
 
-API_KEY = 'AIzaSyBLUuLuM2qPUJrbRNbdD1YLF2mdj-EFFYY'
+from api_key import API_KEY
+
 gmaps = googlemaps.Client(key=API_KEY)
 
-res = gmaps.distance_matrix((42.35044, -71.08945), (42.35083, -71.08981), mode='walking')
-print(res)
 
+time = gmaps.distance_matrix(
+            start_ls,
+            end_ls,
+            mode='driving',
+            traffic_model='optimistic',
+            departure_time=1544000018,
+        )
 
 # load in the data points 
 
-for route in [(67, 179), (179, 80), (178, 80), (67, 53), (53, 67)]:
+all_times = {}
+for route in [(64, 190), (39, 22), (189, 190), (67, 74), (22, 190), (80, 78), (39, 46), (133, 124),
+              (4, 22), (80, 190)]:
+    start, end = route
+    start_points = pd.read_csv(f'sample_points/r{start}.csv', header=0)
+    end_points = pd.read_csv(f'sample_points/r{end}.csv', header=0)
+    num_points = start_points.shape[0]
+    num_splits = math.ceil(num_points/100)
+    print(num_points)
+    print(num_splits)
+    times = []
+    for j in range(num_splits):
+        start_ls = []
+        end_ls = []
+        for i in range(100):
+            start_ls.append((start_points['x'][100*j+i],start_points['y'][100*j+i]))
+            end_ls.append((end_points['x'][100*j+i], end_points['y'][100*j+i]))
+
+        time = gmaps.distance_matrix(
+            start_ls,
+            end_ls,
+            mode='driving',
+            traffic_model='optimistic',
+            departure_time=1544000018,
+        )
+        times.append(time)
+        break
+
+    all_times[route] = times
+
     
+
